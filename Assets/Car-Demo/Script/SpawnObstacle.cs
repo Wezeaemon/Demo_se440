d5b0133d7c79b74b42b75797968091ed272ac4e6
Wezeaemon <shatomon12345@gmail.com>
2024-06-18 20:28:22 +0700

    Update SpawnObstacle Of Wezeamon

--- Assets/Car-Demo/Script/SpawnObstacle.cs
+++ Assets/Car-Demo/Script/SpawnObstacle.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SpawnObstacle : MonoBehaviour
 {
-    [SerializeField] private float spawnInterval = 1f;
+    [SerializeField] private float spawnInterval = 1f; // SpawnObstace : đẻ đối tượng.
     private float _timer;
     
         // Start is called before the first frame update
